1f4f1ea2040ceab462d4d9769cc54a9b77c57e5d
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-02 10:50:34 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * main - Entry point
+ *
+ *description this program about using put to diplay standard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
