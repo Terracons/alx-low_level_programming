f11ab378c224c1918d1e09cd0d7fe686ed3c5602
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-02 11:47:36 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program return printf  function
+ * Return: this program will return(0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
