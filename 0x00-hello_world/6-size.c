2c5a88fa393c6b533c3d805031f182130bb9afae
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-02 15:58:57 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This program return the size of char,int and long
+ * Return: this program return(0)
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	return (0);
+}
