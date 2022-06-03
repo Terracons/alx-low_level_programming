08cc9db9b32fd8082c6329e5daae2e72c47624ce
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-03 09:34:57 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this program is about printing a lower case of an alphabet
+ *
+ * Return: zero (0)
+ */
+int main(void)
+
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
