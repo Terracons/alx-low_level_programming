4ec4ce87da309fb9e2accdf4173d057e0940d090
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-03 13:23:06 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this program is about printing a lower case of an alphabet
+ *
+ * Return: zero (0)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar(10);
+	return (0);
+}
