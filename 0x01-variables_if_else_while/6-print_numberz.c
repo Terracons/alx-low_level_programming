fb94dc89afd7b10379dc88349c170b5ed7335bc5
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-03 13:57:55 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this program is about printing a lower case of an alphabet
+ *
+ * Return: zero (0)
+ */
+int main(void)
+
+{
+	int i = '1';
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
