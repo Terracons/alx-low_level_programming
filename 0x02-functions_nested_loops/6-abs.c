f98bb4cd60fc12b231937fa2e4452cbab6068549
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-08 10:25:56 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _abs - this program print sign
+ *
+ *
+ * @n: character to test
+ * Return: this program return n
+ */
+
+int _abs(int n)
+{
+	int n;
+
+	if (n < 0)
+
+		n = -n;
+	return (n);
+}
