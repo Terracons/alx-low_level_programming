21a4512cba0541fb20f420f30ccffde4a1f7cc9a
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-08 11:55:24 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - this progam return addition
+ * of two
+ * digit of a number
+ *
+ * @a: first number
+ * @b: second number
+ * Return: this program return n
+ */
+
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+
+	return (n);
+}
