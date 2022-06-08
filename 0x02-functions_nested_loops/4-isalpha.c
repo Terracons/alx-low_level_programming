a91af57dc27daa89a26d5c4213c6035fbcf36e53
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-07 21:08:51 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/**
+ * _isalpha - this program return upper
+ * case of an alphabet
+ *
+ *
+ * @c: character to test
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+		return (1);
+	else
+		return (0);
+
+}
