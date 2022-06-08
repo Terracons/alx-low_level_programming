85b8fc990d16a7c8226455b204eee7ececcc70aa
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-07 20:41:23 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+ * _islower - this program return lower
+ * case of an alphabet
+ *
+ *
+ * @c: character to test
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+	else
+		return (0);
+
+}
