03e5e5c75bb6e183a2e9f9abdeedaa78a02877f5
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-07 21:51:12 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int _putchar(char c);
+
+
+/**
+ * print_sign - this program print sign
+ *
+ *
+ * @c: character to test
+ * Return: 1 or 0 or -1 0r /
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+
+}
