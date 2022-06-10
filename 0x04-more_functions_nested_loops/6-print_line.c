f75b2ce9d250c6470db5cd38df7d728b3fa8c97e
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-09 18:43:15 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - this program
+ * print line
+ *
+ * @n: number of character
+ *
+ * Return: return void
+ */
+void print_line(int n)
+{
+	int i
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
