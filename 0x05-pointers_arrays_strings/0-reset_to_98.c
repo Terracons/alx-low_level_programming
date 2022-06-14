c609c8c794fa169c07fc93897356ed630a924a64
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-14 13:31:38 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include < "main.h">
+
+/**
+ * reset_to_98 - this program change the value of
+ * pointr
+ *
+ * @n: pointer parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include < "main.h">
+
+/**
+ * swap_int - this program change the value of
+ * pointr
+ *
+ * @n: pointer paramete
+ * */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
