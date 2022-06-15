398ea3e9f545f06bb39bf4abd62bc732fbe36a6a
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-14 20:37:11 -0700

    strings lenth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - return
+ * the lenth of a strings
+ *
+ * @s: charter s
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		continue;
+	}
+	return (n);
+}
