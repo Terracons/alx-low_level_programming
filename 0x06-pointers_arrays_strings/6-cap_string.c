82d253bce656f11115beed2f6436b0537ec422a0
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-16 07:36:23 -0700

    change cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - this program
+ * chaange lower to upper
+ *
+ * @str: lowerr case
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int a;
+	int b;
+
+	char i[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+
+	a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a == 0 && str[a] >= 97 && str[a] <= 122)
+		{
+			str[a] = str[a] - 32;
+		}
+		b = 0;
+		while (i[b] != '\0')
+		{
+			if (i[b] == str[a] && (str[a + 1] >= 97 && str[a + 1] <= 122))
+			{
+				str[a + 1] = str[a + 1] - 32;
+			}
+			b++;
+		}
+	a++;
+	}
+	return (str);
+}
