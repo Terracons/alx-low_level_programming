9fb362e482f26ff83f7e41b6d13805649dbb1bbc
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-16 06:29:48 -0700

    copy strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - this program
+ * concatenate two strings
+ *
+ * @dest: first strings
+ * @src: second strings
+ * @n: num of chracter
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	int a = 0;
+
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	a = a;
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
