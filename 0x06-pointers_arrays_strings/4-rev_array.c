a1c3aeecf460970703c47dafa6785edfc863457d
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-16 06:57:40 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * reverse_array - this program
+ * reverse array
+ *
+ * @a: array
+ * @n: number of element
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	n = n - 1;
+
+	while (i < n)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+		i++;
+		n--;
+	}
+}
