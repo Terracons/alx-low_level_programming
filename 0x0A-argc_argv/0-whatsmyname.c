521126f6fc5a135a59daf2cfdc8da285f88a6c67
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-06-27 23:35:58 -0700

    arrgumentb count

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this program to test the arg
+ *
+ * @argc: the argument count
+ * @argv: the argument array
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
