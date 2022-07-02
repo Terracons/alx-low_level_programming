2d7b5c671ca867c1d4918b25fde663ccb48fbd8a
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-07-01 20:35:37 -0700

    malloc checked correct betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,16 +4,17 @@
 /**
  * malloc_checked - checked the pointer
  * alocated to memory
+ *
  * @b: parameter b
+ *Return: void
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *p
-	
+	void *p;
+
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
 	return (p);
-
 }
