16c4bd476afdc901d6fc94aabe759b6be2dbb958
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-07-04 12:08:05 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is about dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description - this is a struct
+ * about dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
