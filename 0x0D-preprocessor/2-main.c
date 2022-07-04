920c18cb982a416b5677f1ad885bff8dfd901d3f
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-07-04 04:10:51 -0700

    macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - thid program
+ * return name of the file
+ * Return: 1
+ */
+int main()
+{
+	printf("%s \n", __FILE__ );
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+macro aas funtion
