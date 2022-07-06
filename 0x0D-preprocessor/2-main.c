53a79795e3d72119f8ba74734f15974a1a2c2c5e
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-07-05 20:13:04 -0700

    all correction made

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,8 +5,8 @@
  * return name of the file
  * Return: 1
  */
-int main()
+int main(void)
 {
-	printf("%s \n", __FILE__ );
+	printf("%s \n", __FILE__);
 	return (0);
 }
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,4 @@
-#define ABS(X) (X < 0 ? (-1*X) : X)
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(X) (X < 0 ? (-1 * X) : X)
+#endif
