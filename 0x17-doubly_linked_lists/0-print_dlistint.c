68d7d7f55d952b20d4370550b22aac22716bb773
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-09-01 21:45:12 -0700

    double link list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - print all element of double link list
+ * @h: variable h
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num_nodes = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		num_nodes++;
+	}
+	return (num_nodes)
+}
